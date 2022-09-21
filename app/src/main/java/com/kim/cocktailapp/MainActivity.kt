029d13f087f46cdfa06d5079137ae40ec120a2cc
JosephAkim <Udokimjoseph@gmail.com>
2022-09-21 14:42:51 +0100

    fragments and mainActivity navigation

---
+++ app/src/main/java/com/kim/cocktailapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.kim.cocktailapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //to make the menu switch between navigation component
+        //inflate the bottom navigation and set it up with a navController
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.btm_nav)
+        val navController = Navigation.findNavController(this, R.id.host_fragment)
+
+        NavigationUI.setupWithNavController(bottomNavigation,navController)
+    }
+}
